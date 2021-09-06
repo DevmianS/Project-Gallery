9bb6af0ba285753aec8722ebdb8d3aa8e7e6f5c4
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-09-06 22:21:44 +0200

    added js

---
+++ src/js/script.js
@@ -0,0 +1,56 @@
+let getProjectElem = [0,];
+let getIframe = document.querySelector('iframe');
+let checkIfClicked = false;
+
+
+
+
+
+function setAttributes(el, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
+let maximize = (value) => {
+    
+    if(checkIfClicked == false){
+        getProjectElem[value].style.width = '1000px';
+        getProjectElem[value].style.height = '900px';
+        getProjectElem[value].style.position = 'absolute';
+        getProjectElem[value].style.zIndex = '6';
+        document.getElementById('iframe-'+value).setAttribute('scrolling', 'yes');
+        document.querySelector('.overlay').style.zIndex='1';
+       
+        // document.querySelector('.finished').addEventListener('click', function(){
+        //    maximize();
+        // })
+        checkIfClicked = true;
+
+    }
+    else if(checkIfClicked == true){
+        getProjectElem[value].style.removeProperty('width');
+        getProjectElem[value].style.removeProperty('height');
+        getProjectElem[value].style.removeProperty('z-index');
+        getProjectElem[value].style.removeProperty('position');
+        document.querySelector('.overlay').style.removeProperty('position');
+       
+        
+
+
+        
+        // document.querySelector('body').removeEventListener('click', function(){
+
+        // });
+        checkIfClicked = false;
+    }
+    //setAttributes(getProjectElem[1], )
+};
+
+for(let i = 1; i<=5; i++){
+    getProjectElem[i] = document.getElementById('project-'+i);
+}
+
+for(let i = 1; i<=4; i++){
+    // console.log(getProjectElem[i])
+    getProjectElem[i].addEventListener('click', function(){maximize(i)});
+}
