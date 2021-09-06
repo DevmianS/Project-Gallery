8a950e864ee836c7fe3553e3b93c4d8b76726662
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-09-06 23:00:33 +0200

    save

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 let getProjectElem = [0,];
+let getIframeId = [0,];
 let getIframe = document.querySelector('iframe');
 let checkIfClicked = false;
 
@@ -14,12 +15,13 @@ function setAttributes(el, attrs) {
 let maximize = (value) => {
     
     if(checkIfClicked == false){
-        getProjectElem[value].style.width = '1000px';
-        getProjectElem[value].style.height = '900px';
-        getProjectElem[value].style.position = 'absolute';
+        getProjectElem[value].style.width = '80vw';
+        getProjectElem[value].style.height = '80vh';
+        getProjectElem[value].style.position = 'fixed';
         getProjectElem[value].style.zIndex = '6';
         document.getElementById('iframe-'+value).setAttribute('scrolling', 'yes');
         document.querySelector('.overlay').style.zIndex='1';
+        console.log(document.querySelector('.overlay'))
        
         // document.querySelector('.finished').addEventListener('click', function(){
         //    maximize();
@@ -32,7 +34,9 @@ let maximize = (value) => {
         getProjectElem[value].style.removeProperty('height');
         getProjectElem[value].style.removeProperty('z-index');
         getProjectElem[value].style.removeProperty('position');
+        document.getElementById('iframe-'+value).setAttribute('scrolling', 'no');
         document.querySelector('.overlay').style.removeProperty('position');
+        document.querySelector('.overlay').style.removeProperty('z-index');
        
         
 
@@ -48,6 +52,7 @@ let maximize = (value) => {
 
 for(let i = 1; i<=5; i++){
     getProjectElem[i] = document.getElementById('project-'+i);
+    getIframeId[i] = document.getElementById('iframe-'+i);
 }
 
 for(let i = 1; i<=4; i++){
