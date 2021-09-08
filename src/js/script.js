df5f9f29c3f76bb31859423721912bf7f5dba01e
Dant3s15 <82174072+Dant3s15@users.noreply.github.com>
2021-09-08 13:01:20 +0200

    test

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ let getIframe = document.querySelector('iframe');
 let checkIfClicked = 0;
 let getOverlay = document.querySelectorAll('.overlay');
 
+let addToolbar = document.createElement('div')
 
 
 
@@ -11,7 +12,17 @@ function setAttributes(el, attrs) {
     for(var key in attrs) {
       el.setAttribute(key, attrs[key]);
     }
-  }
+}
+
+function addElements(val){
+    document.getElementById('project-'+val).appendChild(addToolbar);
+    addToolbar.classList.add('toolbar');
+    console.log(addToolbar)
+    
+    //  let text = document.createTextNode('X');
+    //  addToolbar.appendChild(text);
+}
+
 let maximize = (value) => {
     
     if(checkIfClicked == false){
@@ -20,8 +31,10 @@ let maximize = (value) => {
         getProjectElem[value].style.position = 'fixed';
         getProjectElem[value].style.zIndex = '6';
         document.getElementById('iframe-'+value).setAttribute('scrolling', 'yes');
-        document.querySelectorAll('.overlay')[value-1].style.zIndex='1';
+        // document.querySelectorAll('.overlay')[value-1].style.zIndex='1';
+        getProjectElem[value].querySelector('.overlay').style.zIndex='1';
 
+        addElements(value);
        
         // document.querySelector('.finished').addEventListener('click', function(){
         //    maximize();
@@ -35,8 +48,8 @@ let maximize = (value) => {
         getProjectElem[value].style.removeProperty('z-index');
         getProjectElem[value].style.removeProperty('position');
         document.getElementById('iframe-'+value).setAttribute('scrolling', 'no');
-        document.querySelectorAll('.overlay')[value-1].style.removeProperty('position');
-        document.querySelectorAll('.overlay')[value-1].style.removeProperty('z-index');
+        getProjectElem[value].querySelector('.overlay').style.removeProperty('position');
+        getProjectElem[value].querySelector('.overlay').style.removeProperty('z-index');
        
         
 
